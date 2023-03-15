be053713f5c8a53e377c4b28cc7f6e4cb4b178ad
kmn01 <55681293+kmn01@users.noreply.github.com>
2023-03-15 15:18:00 +0530

    graph

--- graph1.js
+++ graph1.js
@@ -1,18 +1,3 @@
-// set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 30, left: 40},
-  width = 500 - margin.left - margin.right,
-  height = 500 - margin.top - margin.bottom;
-
-// append the svg object to the body of the page
-var gp = document.getElementById("my_dataviz")
-var svg = d3.select(gp)
-.append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-.append("g")
-  .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
-
 var data = {
   "nodes": [
     {
@@ -54,6 +39,10 @@ var data = {
     {
       "id": 10,
       "name": "J"
+    },
+    {
+      "id": 11,
+      "name": "K"
     }
   ],
   "links": [
@@ -103,51 +92,91 @@ var data = {
     {
       "source": 5,
       "target": 10
+    },
+    {
+      "source": 6,
+      "target": 11
     }
   ]
 };
 
-// Initialize the links
-var link = svg
-    .selectAll("line")
-    .data(data.links)
-    .enter()
-    .append("line")
-    .style("stroke", "#aaa")
+function run_graph(){
+  // set the dimensions and margins of the graph
+  var margin = {top: 10, right: 30, bottom: 30, left: 40},
+  width = 1000 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
 
-// Initialize the nodes
-var node = svg
-    .selectAll("circle")
-    .data(data.nodes)
-    .enter()
-    .append("circle")
-    .attr("r", 20)
-    .style("fill", "#69b3a2")
+  // append the svg object to the body of the page
+  var gp = document.getElementById("my_dataviz")
+  var svg = d3.select(gp)
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
 
-// Let's list the force we wanna apply on the network
-var simulation = d3.forceSimulation(data.nodes)                 // Force algorithm is applied to data.nodes
-    .force("link", d3.forceLink()                               // This force provides links between nodes
-        .id(function(d) { return d.id; })                     // This provide  the id of a node
-        .links(data.links)                                    // and this the list of links
-    )
-    .force("charge", d3.forceManyBody().strength(-400))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
-    .force("center", d3.forceCenter(width / 2, height / 2))     // This force attracts nodes to the center of the svg area
-    .on("end", ticked);
+  // Initialize the links
+  var link = svg
+  .selectAll("line")
+  .data(data.links)
+  .enter()
+  .append("line")
+  .style("stroke", "#aaa")
 
-// This function is run at each iteration of the force algorithm, updating the nodes position.
-function ticked() {
+  // Initialize the nodes
+  var node = svg
+  .selectAll("circle")
+  .data(data.nodes)
+  .enter()
+  .append("circle")
+  .attr("r", 20)
+  .style("fill", "#69b3a2")
+
+  // Let's list the force we wanna apply on the network
+  var simulation = d3.forceSimulation(data.nodes)                 // Force algorithm is applied to data.nodes
+  .force("link", d3.forceLink()                               // This force provides links between nodes
+      .id(function(d) { return d.id; })                     // This provide  the id of a node
+      .links(data.links)                                    // and this the list of links
+  )
+  .force("charge", d3.forceManyBody().strength(-400))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
+  .force("center", d3.forceCenter(width / 2, height / 2))     // This force attracts nodes to the center of the svg area
+  .on("end", ticked);
+
+  // This function is run at each iteration of the force algorithm, updating the nodes position.
+  function ticked() {
   link
-      .attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
+    .attr("x1", function(d) { return d.source.x; })
+    .attr("y1", function(d) { return d.source.y; })
+    .attr("x2", function(d) { return d.target.x; })
+    .attr("y2", function(d) { return d.target.y; });
 
   node
-      .attr("cx", function (d) { return d.x+6; })
-      .attr("cy", function(d) { return d.y-6; });
+    .attr("cx", function (d) { return d.x+6; })
+    .attr("cy", function(d) { return d.y-6; });
+  }
 }
 
+run_graph();
+node_ctr = 11;
 
 function add_node(){
-
+  node_ctr += 1;
+  source_num = Math.floor(Math.random() * (node_ctr-2)) + 1;
+  var new_node = {
+    "id": node_ctr,
+    "name": "K"
+  }
+  console.log(data.nodes);
+  data.nodes.push(new_node);
+  var new_link = {
+    "source": source_num,
+    "target": node_ctr
+  }
+  data.links.push(new_link);
+  
+  // d3.select("svg").selectAll("*").remove();
+  d3.select("svg").remove();
+  run_graph();
+  console.log(data.nodes), console.log(data.links);
 }
