42d670b12075e71fe9f257c3f96bf857e7c5b332
reshchat <reshmi19441@iiitd.ac.in>
2023-03-15 13:21:22 +0530

    graph page from dictionary

--- graph1.js
+++ graph1.js
@@ -13,49 +13,62 @@ var svg = d3.select(gp)
   .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
 
-d3.json("graph_temp.json", function( data) {
+var data = {
+  "nodes": [
+    {"id": "1", "group": 1},
+    {"id": "2", "group": 1},
+    {"id": "3", "group": 1},
+    {"id": "4", "group": 1},
+    {"id": "5", "group": 1}
+  ],
+  "links": [
+    {"source": "2", "target": "1", "value": 1},
+    {"source": "3", "target": "1", "value": 8},
+    {"source": "4", "target": "1", "value": 10},
+    {"source": "5", "target": "1", "value": 1}
+  ]
+};
 
-  // Initialize the links
-  var link = svg
+// Initialize the links
+var link = svg
     .selectAll("line")
     .data(data.links)
     .enter()
     .append("line")
-      .style("stroke", "#aaa")
+    .style("stroke", "#aaa")
 
-  // Initialize the nodes
-  var node = svg
+// Initialize the nodes
+var node = svg
     .selectAll("circle")
     .data(data.nodes)
     .enter()
     .append("circle")
-      .attr("r", 20)
-      .style("fill", "#69b3a2")
+    .attr("r", 20)
+    .style("fill", "#69b3a2")
 
-  // Let's list the force we wanna apply on the network
-  var simulation = d3.forceSimulation(data.nodes)                 // Force algorithm is applied to data.nodes
-      .force("link", d3.forceLink()                               // This force provides links between nodes
-            .id(function(d) { return d.id; })                     // This provide  the id of a node
-            .links(data.links)                                    // and this the list of links
-      )
-      .force("charge", d3.forceManyBody().strength(-400))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
-      .force("center", d3.forceCenter(width / 2, height / 2))     // This force attracts nodes to the center of the svg area
-      .on("end", ticked);
+// Let's list the force we wanna apply on the network
+var simulation = d3.forceSimulation(data.nodes)                 // Force algorithm is applied to data.nodes
+    .force("link", d3.forceLink()                               // This force provides links between nodes
+        .id(function(d) { return d.id; })                     // This provide  the id of a node
+        .links(data.links)                                    // and this the list of links
+    )
+    .force("charge", d3.forceManyBody().strength(-400))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
+    .force("center", d3.forceCenter(width / 2, height / 2))     // This force attracts nodes to the center of the svg area
+    .on("end", ticked);
 
-  // This function is run at each iteration of the force algorithm, updating the nodes position.
-  function ticked() {
-    link
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+// This function is run at each iteration of the force algorithm, updating the nodes position.
+function ticked() {
+  link
+      .attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
 
-    node
-         .attr("cx", function (d) { return d.x+6; })
-         .attr("cy", function(d) { return d.y-6; });
-  }
+  node
+      .attr("cx", function (d) { return d.x+6; })
+      .attr("cy", function(d) { return d.y-6; });
+}
 
-});
 
 function add_node(){
 
