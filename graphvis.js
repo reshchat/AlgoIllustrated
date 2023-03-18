8b0f93b9be62892de62caf95c41d314621f1692e
kmn01 <55681293+kmn01@users.noreply.github.com>
2023-03-18 20:06:14 +0530

    svg positioning

--- graphvis.js
+++ graphvis.js
@@ -1,7 +1,7 @@
 function run_graph() {
 	// set the dimensions and margins of the graph
-	var width = 900;
-	var height = 520;
+	var width = 1000;
+	var height = 450;
 
 	// append the svg object to the body of the page
 	var svg = d3
