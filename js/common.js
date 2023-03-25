21febcbf9d03d517d5dff2e802ff685cd73a3490
kmn01 <55681293+kmn01@users.noreply.github.com>
2023-03-25 08:08:09 +0530

    menu functionality

--- js/common.js
+++ js/common.js
@@ -38,3 +38,11 @@ $( document ).ready(function() {
 	});
 	
 });
+
+function openNav() {
+	document.getElementById("myNav").style.width = "25%";
+}
+  
+function closeNav() {
+	document.getElementById("myNav").style.width = "0%";
+}
