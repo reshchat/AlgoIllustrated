d803de00bf65aa7b30c5c8269c40a9fa5b14f276
kmn01 <55681293+kmn01@users.noreply.github.com>
2023-04-05 20:58:11 +0530

    button

--- sortingvis.js
+++ sortingvis.js
@@ -49,29 +49,98 @@ var create_array = function create_array(list_part, margin) {
 		.attr("text-anchor", "middle")
 		.attr("alignment-baseline", "middle");
 };
-
 var sorting_divide = function sorting_divide() {
 	var margin = { top: 20, right: 100, bottom: 100, left: width / 2 };
 	var diff = 50;
+    
 	for (let level in lists_down) {
-		var num_elements = lists_down[level].length;
-        lists_down[level].forEach(function (list) {
-            num_elements += list.length;
-		});
-		var total_width = num_elements * nodeWidth;
-		var initial_margin_left = (width - total_width) / 2;
-		margin.left = initial_margin_left;
-		margin.top += diff;
-        var prevlen = 0;
-		lists_down[level].forEach(function (list, i) {
-            var element_width = prevlen*nodeWidth;
-            margin.left += (element_width + 2* nodeWidth);
-            prevlen = list.length;
-            console.log("list", list, "margin", margin);
-			create_array(list, margin);
-		});
+        margin.top += diff;
+        l=lists_down[level].length
+        l2=0
+        if(level>0){
+
+            l2=lists_down[level-1].length
+        }
+        ones=0
+        for (let i=0; i<l; i++) {
+            if (lists_down[level][i].length == 1) {
+                if(level==0){
+                    margin.left=width/((level+1)**2) - lists_down[level][i].length*nodeWidth/2
+                }
+                else{
+                    
+                    for (let j=ones; j<l2; j++) {
+                        if (lists_down[level-1][j].length == 2) {
+                            if(j>0){
+                                margin.left=width/((level-1)**2)*(j+1) -width/((level+1)**2) - lists_down[level][i].length*nodeWidth/2
+                                create_array(lists_down[level][i], margin);
+                                i++
+                                margin.left=width/((level-1)**2)*(j+1) +width/((level+1)**2) - lists_down[level][i].length*nodeWidth/2
+                                create_array(lists_down[level][i], margin);
+                            }
+                            else{
+                                margin.left=width/((level)**2)*(j+1) -width/((level+1)**2) - lists_down[level][i].length*nodeWidth/2
+                                create_array(lists_down[level][i], margin);
+                                i++
+                                margin.left=width/((level)**2)*(j+1) +width/((level+1)**2) - lists_down[level][i].length*nodeWidth/2
+                                create_array(lists_down[level][i], margin);
+                            }
+                            
+                            ones++
+                            break
+                        }
+                        else if (lists_down[level-1][j].length == 3) {
+                            if(j==0){
+
+                                margin.left=width/((level)**2)*(j+1) +width/((level+1)**2) - lists_down[level][i].length*nodeWidth/2
+                            }
+                            else{
+                                margin.left=width/((level-1)**2)*(j+1) -width/((level+1)**2) - lists_down[level][i].length*nodeWidth/2
+                            }
+                            create_array(lists_down[level][i], margin);
+                            ones++
+                            break
+                        }
+                    }
+                }
+                
+            }
+            else{
+                if(i==0){
+                margin.left=width/((level+1)**2)*(i+1) - lists_down[level][i].length*nodeWidth/2}
+                else{
+                    margin.left=width/((level)**2)*(i+1) - lists_down[level][i].length*nodeWidth/2
+                }
+                create_array(lists_down[level][i], margin);
+            }
+        }
+			
+		
 	}
 };
+
+// var sorting_divide = function sorting_divide() {
+// 	var margin = { top: 20, right: 100, bottom: 100, left: width / 2 };
+// 	var diff = 50;
+// 	for (let level in lists_down) {
+// 		var num_elements = lists_down[level].length;
+//         lists_down[level].forEach(function (list) {
+//             num_elements += list.length;
+// 		});
+// 		var total_width = num_elements * nodeWidth;
+// 		var initial_margin_left = (width - total_width) / 2;
+// 		margin.left = initial_margin_left;
+// 		margin.top += diff;
+//         var prevlen = 0;
+// 		lists_down[level].forEach(function (list, i) {
+//             var element_width = prevlen*nodeWidth;
+//             margin.left += (element_width + 2* nodeWidth);
+//             prevlen = list.length;
+//             console.log("list", list, "margin", margin);
+// 			create_array(list, margin);
+// 		});
+// 	}
+// };
 
 // var sorting_divide = function sorting_divide() {
 // 	var margin = { top: 20, right: 100, bottom: 100, left: width / 2 };
