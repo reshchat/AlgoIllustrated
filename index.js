d7fb01b3e0ebafbd48cd08f337e17c9d171a404f
kmn01 <55681293+kmn01@users.noreply.github.com>
2023-04-14 12:47:33 +0530

    templates part 1

--- index.js
+++ index.js
@@ -3,10 +3,45 @@ const express = require('express'); //importing the express dependency
 const app = express();              //instantiate an express app, the main work horse of this server
 const port = 3030;                  //port number where server will be listening
 
+app.set('view engine', 'ejs');      //set the view engine to ejs, this will allow us to use ejs templating
+
 //expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
     res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
                                                         //the .sendFile method needs the absolute path to the file 
+});
+
+app.get('/', (req, res) => {       
+    res.render('pages/home');    
+                                                       
+});
+app.get('/graph_simulation', (req, res) => {       
+    res.render('pages/graph_simulation');    
+                                                       
+});
+app.get('/graph_interaction', (req, res) => {       
+    res.render('pages/graph_interaction');    
+                                                       
+});
+app.get('/dp_simulation', (req, res) => {       
+    res.render('pages/dp_simulation');    
+                                                       
+});
+app.get('/dp_interaction', (req, res) => {       
+    res.render('pages/dp_interaction');    
+                                                       
+});
+app.get('/stacks', (req, res) => {       
+    res.render('pages/stacks');    
+                                                       
+});
+app.get('/sorting_simulation', (req, res) => {       
+    res.render('pages/sorting_simulation');    
+                                                       
+});
+app.get('/sorting_interaction', (req, res) => {       
+    res.render('pages/sorting_interaction');    
+                                                       
 });
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
