8db0ccf43faff3858b670228dbd2d930725db2cb
kmn01 <55681293+kmn01@users.noreply.github.com>
2023-04-14 20:12:35 +0530

    homepage

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const port = 3030;                  //port number where server will be listening
 app.set('view engine', 'ejs');      //set the view engine to ejs, this will allow us to use ejs templating
 
 //expression in express to route and respond to a client request
-app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-                                                        //the .sendFile method needs the absolute path to the file 
-});
+// app.get('/', (req, res) => {        //get requests to the root ("/") will route here
+//     res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
+//                                                         //the .sendFile method needs the absolute path to the file 
+// });
 
 app.get('/', (req, res) => {       
     res.render('pages/home');    
