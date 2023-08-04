a2699821a0f0445c90c3fb013bc61161b45bcbf3
kmn01 <55681293+kmn01@users.noreply.github.com>
2023-08-04 13:43:47 +0530

    audios for each page

--- audio.js
+++ audio.js
@@ -1,5 +1,14 @@
-var play_graph_audio = function play_graph_audio() {
-    console.log("play_graph_audio");
-    var audio = document.getElementById("audio_file");
+var play_audio = function play_audio(page) {
+    console.log('page ' + page);
+    var audio;
+    if(page.includes("graph")) {
+        audio = document.getElementById("graph_audio_file");
+    }
+    else if(page.includes("dp")) {
+        audio = document.getElementById("dp_audio_file");
+    }
+    else if(page.includes("stacks")) {
+        audio = document.getElementById("stacks_audio_file");
+    }
     audio.play();
 }
